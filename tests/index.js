2c148225db0ec8470f6c3f39f374c05c79f141a3
sethvincent <sethvincent@gmail.com>
2018-05-06 12:10:55 -0700

    fix tests for ci

--- tests/index.js
+++ tests/index.js
@@ -1,16 +1,20 @@
 var path = require('path')
+var fs = require('fs')
 var test = require('tape')
+var mkdirp = require('mkdirp')
+var rimraf = require('rimraf')
 
 var findRepositories = require('../index')
 
 test('example', function (t) {
   var dir = path.join(__dirname, 'fixtures', 'basic')
+  makeExampleDirectories(dir)
+
   var stream = findRepositories(dir, {})
   var count = 0
 
   stream.on('data', function (data) {
     count++
-    console.log(data)
     t.ok(data.filepath, 'filepath exists on data object')
     t.ok(data.gitState && typeof data.gitState === 'object', 'gitState exists on data object')
     t.ok(data.files && Array.isArray(data.files), 'files array exists on data object')
@@ -18,6 +22,13 @@ test('example', function (t) {
 
   stream.on('end', function () {
     t.ok(count === 2)
+    rimraf.sync(dir)
     t.end()
   })
 })
+
+function makeExampleDirectories (baseDir) {
+  mkdirp.sync(path.join(baseDir, 'a', '.git'))
+  mkdirp.sync(path.join(baseDir, 'b', '.git'))
+  mkdirp.sync(path.join(baseDir, 'c',))
+}
